5650b51c27fc513fad611e412ca5fc3fd9b09dc5
Mikael Matveinen <mikael.matveinen@gmail.com>
2019-01-25 22:06:10 +0200

    The player now moves within the constraints set up in the player mover

--- 6x4/Assets/Game/Scripts/Gameplay/PlayerMover.cs
+++ 6x4/Assets/Game/Scripts/Gameplay/PlayerMover.cs
@@ -5,21 +5,35 @@ namespace Game.Scripts.Gameplay
 	using UnityEngine;
 
 	public class PlayerMover : MonoBehaviour
-    {
-        [SerializeField] Animator animator = default;
-        [SerializeField] float moveSpeed = default;
+	{
+		[SerializeField] Animator animator = default;
+		[SerializeField] float moveSpeed = default;
+		[SerializeField] AnimationCurve movementCurve = default;
+		[SerializeField] float minX = default;
+		[SerializeField] float maxX = default;
+		[SerializeField] float yPos = default;
+		[SerializeField] float zPos = -0.1f;
+
 		static readonly int WALK = Animator.StringToHash("Walk");
 
+
 		public IEnumerator MoveToPosition(Vector3 newPosition)
-        {
+		{
+			newPosition.x = Mathf.Clamp(newPosition.x, this.minX, this.maxX);
+			newPosition.y = this.yPos;
+			newPosition.z = this.zPos;
+
 			this.animator.SetTrigger(WALK);
 
 			var playerTransform = this.transform;
-            var position = playerTransform.position;
-            var moveDuration = (newPosition - position).magnitude / this.moveSpeed;
-            yield return Tweening.TweenPosition(
-				playerTransform, position, newPosition,
-                AnimationCurve.Linear(0, 0, 1, 1), moveDuration);
-        }
-    }
+			var position = playerTransform.position;
+			var moveDuration = (newPosition - position).magnitude / this.moveSpeed;
+			yield return Tweening.TweenPosition(
+				playerTransform,
+				position,
+				newPosition,
+				this.movementCurve,
+				moveDuration);
+		}
+	}
 }
