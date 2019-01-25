ff2ecb23b056179e32ba6c01b9476178a10e23a1
Mikael Matveinen <mikael.matveinen@gmail.com>
2019-01-25 22:17:01 +0200

    Did some reorganizing & Tweaked player movements

--- 6x4/Assets/Game/Scripts/Gameplay/PlayerMover.cs
+++ 6x4/Assets/Game/Scripts/Gameplay/PlayerMover.cs
@@ -14,7 +14,8 @@ namespace Game.Scripts.Gameplay
 		[SerializeField] float yPos = default;
 		[SerializeField] float zPos = -0.1f;
 
-		static readonly int WALK = Animator.StringToHash("Walk");
+		static readonly int ANIMATION_WALKING = Animator.StringToHash("Walking");
+		static readonly int ANIMATION_FACING_LEFT = Animator.StringToHash("FacingLeft");
 
 
 		public IEnumerator MoveToPosition(Vector3 newPosition)
@@ -23,10 +24,12 @@ namespace Game.Scripts.Gameplay
 			newPosition.y = this.yPos;
 			newPosition.z = this.zPos;
 
-			this.animator.SetTrigger(WALK);
-
 			var playerTransform = this.transform;
 			var position = playerTransform.position;
+			
+			this.animator.SetBool(ANIMATION_FACING_LEFT, newPosition.x <= position.x);
+			this.animator.SetBool(ANIMATION_WALKING, true);
+
 			var moveDuration = (newPosition - position).magnitude / this.moveSpeed;
 			yield return Tweening.TweenPosition(
 				playerTransform,
@@ -34,6 +37,8 @@ namespace Game.Scripts.Gameplay
 				newPosition,
 				this.movementCurve,
 				moveDuration);
+
+			this.animator.SetBool(ANIMATION_WALKING, false);
 		}
 	}
 }
