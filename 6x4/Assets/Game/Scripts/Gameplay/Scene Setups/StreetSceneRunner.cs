de317b8eedbe449e5c7463b7adeefa458d32388f
Mikael Matveinen <mikael.matveinen@gmail.com>
2019-01-26 18:45:48 +0200

    All the scenes now have a SceneRunner

---
+++ 6x4/Assets/Game/Scripts/Gameplay/Scene Setups/StreetSceneRunner.cs
@@ -0,0 +1,15 @@
+namespace Game.Scripts.Gameplay.SceneSetups
+{
+	using System.Collections;
+	using TMPro;
+	using UnityEngine;
+	using Zenject;
+
+	public class StreetSceneRunner : SceneRunner
+	{
+		protected override IEnumerator HandleResult(int playerChoice)
+		{
+			yield break;
+		}
+	}
+}
