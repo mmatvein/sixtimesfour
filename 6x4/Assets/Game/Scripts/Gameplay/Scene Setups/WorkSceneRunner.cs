8cf97c0e7aa7cc57100ffdf48fba26c1e2cb98eb
Mikael Matveinen <mikael.matveinen@gmail.com>
2019-01-27 13:59:38 +0200

    Work scene

--- 6x4/Assets/Game/Scripts/Gameplay/Scene Setups/WorkSceneRunner.cs
+++ 6x4/Assets/Game/Scripts/Gameplay/Scene Setups/WorkSceneRunner.cs
@@ -7,9 +7,18 @@ namespace Game.Scripts.Gameplay.SceneSetups
 
 	public class WorkSceneRunner : SceneRunner
 	{
+		[SerializeField] Vector3 computerPos = default;
+		[Inject] PlayerMover playerMover = default;
 		protected override IEnumerator HandleResult(PlayerChoice choice, int choiceValue)
 		{
-			yield break;
+			if (choice == PlayerChoice.Coworker)
+			{
+				yield return new WaitForSeconds(0.5f);
+			}
+			else if (choice == PlayerChoice.Computer)
+			{
+				yield return this.playerMover.MoveToPosition(this.computerPos);
+			}
 		}
 	}
 }
